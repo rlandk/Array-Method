caaaa4d0f23253707bb118c09ba4d4f8e815f460
rlandk <orlandorolland33@gmail.com>
2023-03-17 10:25:43 +0700

    Array Method

---
+++ method.js
@@ -0,0 +1,59 @@
+// array filter
+// let numbers = [1, 2, 3, 4, 5];
+// let evenNumbers = numbers.filter(function (num) {
+//     return num % 2 === 0;
+// });
+// document.writeln(evenNumbers);
+
+// array for each
+// let numbers = [1, 2, 3, 4, 5];
+// numbers.forEach(function (num) {
+// document.writeln(num);
+// });
+
+// array join
+// let fruits = ['apple', 'banana', 'orange'];
+// let fruitString = fruits.join(', ');
+// document.writeln(fruitString);
+
+// array join
+// let fruits = ['apple', 'banana', 'orange'];
+// let fruitString = fruits.join(', ');
+// document.writeln(fruitString);
+
+// array map
+// let numbers = [1, 2, 3, 4, 5];
+// let doubledNumbers = numbers.map(function (num) {
+//     return num * 2;
+// });
+// document.writeln(doubledNumbers);
+
+// array pop
+// let fruits = ['apple', 'banana', 'orange'];
+// let removedFruit = fruits.pop();
+// document.writeln(removedFruit);
+// document.writeln(fruits);
+
+// array push
+// let fruits = ['apple', 'banana'];
+// let newLength = fruits.push('orange', 'mango');
+// document.writeln(newLength);
+// document.writeln(fruits);
+
+// array sort
+// let fruits = ['orange', 'banana', 'apple', 'mango'];
+// fruits.sort();
+// document.writeln(fruits); 
+
+// array for in
+// let person = { name: 'John', age: 30, city: 'New York' };
+// for (let prop in person) {
+//     document.writeln(prop + ': ' + person[prop]);
+// }
+
+// array for of
+let numbers = [1, 2, 3, 4, 5];
+for (let num of numbers) {
+    document.writeln(num);
+}
+
